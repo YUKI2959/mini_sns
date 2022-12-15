bf6fd2fea589a0161c534918393fc57ef9cddf17
内田有貴 <117145436+YUKI2959@users.noreply.github.com>
2022-12-15 15:25:52 +0900

    レスポンスの把握

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   devise_for :users
   root 'users#index'  
   resources :users, only: :new  
